9945ef604d180f25d47ba2f7604f3759cdf86ff7
Sistemas3 <Sistemas3@SISTEMAS3-PC>
2017-10-10 12:58:01 -0500

    10/10/2017
    Funciona el reporte

--- CalculoHoras.aspx
+++ CalculoHoras.aspx
@@ -19,60 +19,24 @@
          #derecho{
              float:right;
         }
-        .auto-style4 {
-            width: 174px;
-            height: 56px;
+      
+        .auto-style1 {
+            width: 463px;
         }
-        .auto-style5 {
-            height: 56px;
+        .auto-style2 {
+            height: 1521px;
         }
-        .auto-style9 {
-        width: 953px;
-        height: 702px;
-        margin-bottom: 0px;
-    }
-    .auto-style10 {
-        width: 473px;
-    }
-        .auto-style12 {
-            width: 466px;
-            height: 56px;
-        }
-        .auto-style14 {
-            width: 394px;
-        }
-        .auto-style16 {
-            width: 395px;
-        }
-        .auto-style17 {
-            width: 376px;
-        }
-        .auto-style18 {
-            width: 258px;
-            height: 56px;
-        }
-        .auto-style19 {
-            height: 56px;
-            width: 141px;
-        }
-        .auto-style20 {
-            width: 141px;
-        }
-        .auto-style22 {
-            width: 466px;
-        }
-        .auto-style23 {
-            width: 174px
-        }
+      
         </style>
-    <div id="contenedor" class="auto-style9">
-    <h3>Calculo de Horas </h3>
-    <div id="izquierdo" class="auto-style10">
-        <table class="auto-style16">
+    <div id="contenedor" class="auto-style2">
+    <h3>Cálculo de Horas </h3>
+    <div id="izquierdo">
+        <table>
             <tr>
                 <td class="auto-style22">Sucursal:<br />
                     <uc1:wucsucursales ID="wucSucursales" runat="server" /></td>
               <td class="auto-style23">Empleado:<asp:TextBox ID="grdSR" runat="server" Visible="false" Width="176px"></asp:TextBox>
+                  <asp:TextBox ID="idEmpleadoTX" runat="server" Visible="False" Width="46px"></asp:TextBox>
                   <br />
                   <uc2:wucempleados2 ID="wucEmpleados2" runat="server" />  
             </tr>
@@ -84,7 +48,7 @@
                 
 
                  </td>
-                <td class="auto-style4">Fecha Fin:<br />
+                <td>Fecha Fin:<br />
                     <asp:TextBox ID="TxFechaFin" runat="server" MaxLength="40" Width="100px" CssClass="txtCaptura" Height="24px" Enabled="False" />
 
                     <asp:ImageButton ID="ImageButton2" runat="server" Height="18px" ImageUrl="~/img/favicon.ico" Width="19px" />
@@ -95,7 +59,7 @@
                     </td>
             </tr>  
             <tr>
-                <td ><asp:Calendar ID="FIngreso" runat="server" BackColor="White" BorderColor="#3366CC" BorderWidth="1px" CellPadding="1" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="#003399" Height="76px" Width="152px" TitleFormat="Month" Visible="False" >
+                <td class="auto-style25" ><asp:Calendar ID="FIngreso" runat="server" BackColor="White" BorderColor="#3366CC" BorderWidth="1px" CellPadding="1" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="#003399" Height="76px" Width="152px" TitleFormat="Month" Visible="False" >
                     <DayHeaderStyle BackColor="White" ForeColor="#336666" Height="1px" />
                     <NextPrevStyle Font-Size="8pt" ForeColor="#CCCCFF" />
                     <OtherMonthDayStyle ForeColor="#999999" />
@@ -108,7 +72,7 @@
                     <asp:label ID="Lmsg" runat="server" CssClass="error"></asp:label>
 
                 </td>
-               <td ><asp:Calendar ID="FFinal" runat="server" BackColor="White" BorderColor="#3366CC" BorderWidth="1px" CellPadding="1" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="#003399" Height="76px" Width="152px" TitleFormat="Month" Visible="False" >
+               <td class="auto-style25" ><asp:Calendar ID="FFinal" runat="server" BackColor="White" BorderColor="#3366CC" BorderWidth="1px" CellPadding="1" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="#003399" Height="76px" Width="152px" TitleFormat="Month" Visible="False" >
                     <DayHeaderStyle BackColor="White" ForeColor="#336666" Height="1px" />
                     <NextPrevStyle Font-Size="8pt" ForeColor="#CCCCFF" />
                     <OtherMonthDayStyle ForeColor="#999999" />
@@ -126,17 +90,40 @@
             </table>
         <table>
             <tr>
+                <td>  
+                </td>
                 <td>
                   <asp:GridView ID="GridView1" runat="server" 
             DataKeyNames ="chec" AutoGenerateColumns="False" CellPadding="4" 
-            ForeColor="#333333" GridLines="None" Width="455px" Height="92px" AllowPaging="True" OnPageIndexChanging="GridView1_PageIndexChanging1" PageSize="15">
+            ForeColor="#333333" GridLines="None" Width="537px" Height="303px" AllowPaging="True" OnPageIndexChanging="GridView1_PageIndexChanging1" PageSize="12">
             <Columns>
                 <asp:BoundField DataField="chec" ItemStyle-Width="1" ItemStyle-Font-Size="1" > 
                 <ItemStyle Font-Size="1pt" Width="1px"></ItemStyle>
                 </asp:BoundField>
+
                 <asp:ButtonField ButtonType="Image" CommandName="Editar" ImageUrl="~/Imagenes/editar.png" />
                 <asp:BoundField DataField="chec" HeaderText="Fecha y Hora" SortExpression="chec" />
                 <asp:BoundField DataField="tipo" HeaderText="Tipo" SortExpression="tipo" />
+                <asp:TemplateField HeaderText="Incidencia">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                       
+                      <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="incidencia" DataValueField="incidencia"></asp:DropDownList>
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:StarTconnStrRH %>" SelectCommand="SELECT [incidencia] FROM [Incidencia] order by  [Incidencia] asc "></asp:SqlDataSource>
+                    </ItemTemplate>
+                </asp:TemplateField>
+               <asp:TemplateField HeaderText="Observacion">
+                        <ItemTemplate>
+                            <asp:Label Text='' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtObservacion" Text='' runat="server" />
+                        </EditItemTemplate>
+                       
+                    </asp:TemplateField>
+                   <asp:ButtonField ButtonType="Image" CommandName="Guardar" ImageUrl="~/imagenes/seleccionar.png" />
            </Columns>
             <HeaderStyle BackColor="#f39c12" ForeColor="#f8f8f8" />
             <RowStyle BackColor="#f3f3f3" ForeColor="#333333" />
@@ -145,14 +132,20 @@
             <FooterStyle BackColor="#FF9933" Font-Size="1" Height="1" />
             <PagerStyle BackColor="#3088b0" ForeColor="#333333" HorizontalAlign="Center" />
         </asp:GridView>
+                
+                
+                
+                
+                
                 </td>
             </tr>
         </table>
+        <br />
     </div> <!-- listaDatos -->
 
 
-    <div id="derecho" class="auto-style14">
-        <table class="auto-style17">                 
+    <div id="derecho" class="auto-style1">
+        <table>                 
             <tr>
                
                 <td class="auto-style19">Horas Totales:<br />
@@ -161,40 +154,60 @@
                     <asp:TextBox ID="TxHorasTrabajadas" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
                  <td class="auto-style5">
                      
-                    <asp:Button ID="btnGenerar" runat="server" CssClass="btn btn-danger btn-block btn-flat" Text="Generar"  ToolTip="Generar" Width="100px" />               
-                    
-                 </td>
+                     &nbsp;</td>
             </tr>
             <tr>
-                <td class="auto-style20">Dias Descansados:<br />
+                <td>Dias Descansados:<br />
                     <br />
-                    <asp:TextBox ID="TextBox1" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
-                <td class="auto-style18">Dias Descansados<br />
-                    &nbsp;Trabajados:<asp:TextBox ID="TextBox2" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
+                    <asp:TextBox ID="TxDDescasados" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
+                <td>Dias Descansados<br />
+                    &nbsp;Trabajados:<br />
+                    <asp:TextBox ID="TextBox2" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
             </tr>
             <tr>
-                <td class="auto-style20">Dias Festivos Trabajados:<br />
+                <td>Dias Festivos 
+                    <br />
+                    Trabajados:<br />
                     <asp:TextBox ID="TextBox3" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
-                <td class="auto-style18">Horas Extras:<br />
+                <td>Horas Extras:<br />
                     <br />
-                    <asp:TextBox ID="TextBox4" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
+                    <asp:TextBox ID="TxHorasExtras" runat="server" CssClass="txtCaptura" MaxLength="40" Width="110px" /></td>
             </tr>
             </table>
 
         <table>
             <tr>
-                <td>  <asp:Button ID="btnReporte" runat="server" CssClass="btn btn-success btn-block btn-flat" Text="Reporte"  ToolTip="Reporte" Width="100px" /></td>
-            </tr>
-        </table>
-
-         <table>
-            <tr>
-                <rsweb:ReportViewer ID="ReportViewer1" runat="server" Height="367px"></rsweb:ReportViewer>
+                <td>  
+                     
+                    <br />
+                     
+                    <asp:Button ID="btnGenerar" runat="server" CssClass="btn btn-danger btn-block btn-flat" Text="Generar"  ToolTip="Generar" Width="101px" />               
+                    
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td>
+            <td>  
+                <br />
+                <asp:Button ID="btnReporte" runat="server" CssClass="btn btn-success btn-block btn-flat" Text="Reporte"  ToolTip="Reporte" Width="108px" /></td>
             </tr>
         </table>
     </div>
         
-    
+    <rsweb:ReportViewer ID="reporte" runat="server" Font-Names="Verdana" Font-Size="8pt" Height="554px" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt" Width="890px">
+        <LocalReport ReportPath="Report1.rdlc">
+            <DataSources>
+                <rsweb:ReportDataSource DataSourceId="ObjectDataSource1" Name="DataSet1" />
+            </DataSources>
+        </LocalReport>
+        </rsweb:ReportViewer>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" OldValuesParameterFormatString="original_{0}" SelectMethod="GetDatahecIncidencia" TypeName="nomRHDataSetTableAdapters.vm_ChequeoIncidenciaTableAdapter">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="idEmpleadoTX" Name="idempleado" PropertyName="Text" Type="Int32" />
+                <asp:ControlParameter ControlID="TxFechaInicio" Name="Fech1" PropertyName="Text" Type="DateTime" />
+                <asp:ControlParameter ControlID="TxFechaFin" Name="Fech2" PropertyName="Text" Type="DateTime" />
+            </SelectParameters>
+        </asp:ObjectDataSource>
    </div>
+      
 </asp:Content>
+
 
--- CalculoHoras.aspx.vb
+++ CalculoHoras.aspx.vb
@@ -1,32 +1,13 @@
 ﻿
 Imports System.Data.SqlClient
+Imports System.Globalization
+Imports Microsoft.Reporting.WebForms
 Imports RHLogica
 
 Partial Class CalculoHoras
     Inherits System.Web.UI.Page
     Public gvPos As Integer
-    'Datos de los campos de texto
-    Dim FIn As Date
-    Dim FFn As Date
 
-    'Variable global
-    Dim Fech As Date
-    Dim FechCorta As String
-
-    'Variables para operaciones
-    Dim HoraIn As Date
-    Dim HoraFn As Date
-
-    Dim HI As Integer
-    Dim HF As Integer
-
-    Dim MI As Integer
-    Dim MF As Integer
-    ' Dim tsDiferencia As TimeSpan = TimeSpan.Zero
-    Dim tsDiferencia As Integer
-    ' Dim Acum As TimeSpan = TimeSpan.Zero
-    Dim Acum As Integer
-    Dim con As Integer
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
         If IsNothing(Session("usuario")) Then Response.Redirect("Default.aspx", True)
         If Not Page.IsPostBack Then
@@ -35,6 +16,22 @@ Partial Class CalculoHoras
         End If
         Lmsg.Text = "" : gvPos = 0
         Session("idz_e") = ""
+
+        reporte.ServerReport.Refresh()
+
+
+    End Sub
+    Protected Sub wucEmpleados_empleadoSeleccionada(sender As Object, e As System.EventArgs) Handles wucEmpleados2.empleadoSeleccionado
+
+        idEmpleadoTX.Text = wucEmpleados2.idEmpleado.ToString
+
+        Dim dsP As New ctiCatalogos
+        Dim sr As String()
+
+        sr = dsP.datosEmpleado(wucEmpleados2.idEmpleado)
+
+        '  TxEmpleado.Text = sr(0)
+        ' TxEmpleado.Text = wucEmpleados
     End Sub
     Protected Sub wucSucursales_sucursalSeleccionada(sender As Object, e As System.EventArgs) Handles wucSucursales.sucursalSeleccionada
         Dim gvds As New ctiWUC
@@ -70,12 +67,19 @@ Partial Class CalculoHoras
         FFinal.Visible = False
     End Sub
     Protected Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
-
         If wucEmpleados2.idEmpleado <> 0 Then
             If TxFechaInicio.Text <> "" And TxFechaFin.Text <> "" Then
+
+
+                Dim FechaFinal As Date
+                Dim FechaFinal2 As Date
+                FechaFinal = Convert.ToDateTime(TxFechaFin.Text)
+                FechaFinal2 = DateAdd(DateInterval.Day, 1, FechaFinal).ToString("yyyy-MM-dd")
+
+
                 Dim ec As New ctiCalculo
-                GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-dd-MM"), Format(CDate(TxFechaFin.Text), "yyyy-dd-MM"))
-                ' GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd"), Format(CDate(TxFechaFin.Text), "yyyy-MM-dd"))
+                GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-dd-MM"), Format(CDate(FechaFinal2), "yyyy-dd-MM"), 3)
+                ' GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd"), Format(CDate(FechaFinal), "yyyy-MM-dd"))
 
                 ec = Nothing
                 GridView1.DataBind()
@@ -85,23 +89,38 @@ Partial Class CalculoHoras
         Else
             Lmsg.Text = "Error: Falta Capturar Empleado"
         End If
-
-
     End Sub
     Protected Sub GridView1_PageIndexChanging1(sender As Object, e As GridViewPageEventArgs)
         GridView1.PageIndex = e.NewPageIndex
         Dim ec As New ctiCalculo
-        GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-dd-MM"), Format(CDate(TxFechaFin.Text), "yyyy-dd-MM"))
+        GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-dd-MM"), Format(CDate(TxFechaFin.Text), "yyyy-dd-MM"), 3)
         ec = Nothing
         GridView1.DataBind()
     End Sub
     Protected Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
         HTotales()
-        'HTrabajadas()
+        HTrabajadas()
+        HorasExtras()
         'DDescansados()
         'DDescansadosTrabajados()
     End Sub
     Public Sub HTotales()
+        'Datos de los campos de texto
+        Dim FIn As Date
+        Dim FFn As Date
+
+        'Variable global
+        Dim Fech As Date
+
+        'Variables para operaciones
+        Dim HoraIn As Date
+        Dim HoraFn As Date
+
+        Dim HI As Integer
+        Dim HF As Integer
+        Dim tsDiferencia As Integer
+        ' Dim Acum As TimeSpan = TimeSpan.Zero
+        Dim Acum As Integer
         'Asignar los datos de los campos de texto a Variables
         FIn = Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd")
         FFn = Format(CDate(TxFechaFin.Text), "yyyy-MM-dd")
@@ -111,23 +130,26 @@ Partial Class CalculoHoras
 
         'Inicio del ciclo de comparacion
         While (Fech <= FFn)
+            'Borramos datos de las variables
+            tsDiferencia = 0
+            HI = 0
+            HF = 0
             'Conexion y busqueda de registros
             Using dbC As New SqlConnection
                 dbC.ConnectionString = ConfigurationManager.ConnectionStrings("StarTconnStrRH").ToString
                 dbC.Open()
-                Dim cmd As New SqlCommand("Select inicio,fin From vm_Jornada where fecha>=@fecha AND fecha <= '" & DateAdd(DateInterval.Day, 1, Fech).ToString("yyyy-dd-MM") & "' AND idempleado=@idempleado Order BY fecha ", dbC)
+                Dim cmd As New SqlCommand("Select inicio,fin From vm_Jornada where idempleado=@idempleado AND fecha BETWEEN '" & Fech.ToString("yyyy-MM-dd") & "'  AND '" & DateAdd(DateInterval.Day, 1, Fech).ToString("yyyy-MM-dd") & "' Order BY fecha", dbC)
                 'Dim cmd As New SqlCommand("Select * From Chequeo where chec>=@chec AND chec <= '" & DateAdd(DateInterval.Day, 1, Fech).ToString("yyyy-MM-dd") & "' AND idempleado=@idempleado Order BY chec ", dbC)
 
                 cmd.Parameters.AddWithValue("idempleado", wucEmpleados2.idEmpleado)
                 cmd.Parameters.AddWithValue("fecha", Fech)
                 Dim rdr As SqlDataReader = cmd.ExecuteReader
                 Dim dsP As String()
-                While rdr.Read
+                If rdr.Read Then
                     'Lectura de registros
                     ReDim dsP(3)
                     dsP(0) = rdr("inicio").ToString
                     dsP(1) = rdr("fin").ToString
-
 
                     HoraIn = dsP(0)
                     HoraFn = dsP(1)
@@ -137,12 +159,13 @@ Partial Class CalculoHoras
                     'Diferencia de horas
                     tsDiferencia = 0
                     tsDiferencia = HF - HI
-                End While
-
-                'Limpiar variables
-                HI = 0
-                HF = 0
-
+                    'Limpiar variables
+                    HI = 0
+                    HF = 0
+                    If tsDiferencia < 1 Then
+                        tsDiferencia = 0
+                    End If
+                End If
                 rdr.Close() : rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
             End Using
             'Acumulador de fecha
@@ -151,10 +174,25 @@ Partial Class CalculoHoras
             Acum = tsDiferencia + Acum
             tsDiferencia = 0
         End While
-        TxHorasTrabajadas.Text = Acum
-
+        TxHtotales.Text = Acum
     End Sub
     Public Sub HTrabajadas()
+        'Datos de los campos de texto
+        Dim FIn As Date
+        Dim FFn As Date
+
+        'Variable global
+        Dim Fech As Date
+        'Variables para operaciones
+        Dim HoraIn As Date
+        Dim HoraFn As Date
+        Dim HI As Integer
+        Dim HF As Integer
+        Dim MI As Integer
+        Dim MF As Integer
+        Dim tsDiferencia As Integer
+        Dim Acum As Integer
+
         'Asignar los datos de los campos de texto a Variables
         FIn = Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd")
         FFn = Format(CDate(TxFechaFin.Text), "yyyy-MM-dd")
@@ -163,7 +201,7 @@ Partial Class CalculoHoras
         Fech = FIn
 
         'Inicio del ciclo de comparacion
-        While (Fech < FFn)
+        While (Fech <= FFn)
             'Borramos datos de las variables
             tsDiferencia = 0
             HI = 0
@@ -240,6 +278,166 @@ Partial Class CalculoHoras
     Public Sub DDescansadosTrabajados()
 
     End Sub
+    Public Sub HorasExtras()
+        Dim A, B As Integer
+        A = Convert.ToInt32(TxHorasTrabajadas.Text)
+        B = Convert.ToInt32(TxHtotales.Text)
+        TxHorasExtras.Text = A - B
+    End Sub
+
+    'Protected Sub GVCalculoH_RowCancelingEdit(sender As Object, e As GridViewCancelEditEventArgs) Handles GVCalculoH.RowCancelingEdit
+    '    'Dim FechaFinal As Date
+    '    'Dim FechaFinal2 As Date
+    '    'FechaFinal = Convert.ToDateTime(TxFechaFin.Text)
+    '    'FechaFinal2 = DateAdd(DateInterval.Day, 1, FechaFinal).ToString("yyyy-MM-dd")
 
 
+    '    'Dim ec As New ctiCalculo
+    '    'GVCalculoH.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-dd-MM"), Format(CDate(FechaFinal2), "yyyy-dd-MM"), 3)
+    '    '' GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd"), Format(CDate(FechaFinal), "yyyy-MM-dd"))
+
+    '    'ec = Nothing
+    '    'GVCalculoH.DataBind()
+
+    '    'GVCalculoH.EditIndex = -1
+
+    'End Sub
+    'Protected Sub GVCalculoH_RowEditing(sender As Object, e As GridViewEditEventArgs) Handles GVCalculoH.RowEditing
+    'Dim FechaFinal As Date
+    'Dim FechaFinal2 As Date
+    'FechaFinal = Convert.ToDateTime(TxFechaFin.Text)
+    'FechaFinal2 = DateAdd(DateInterval.Day, 1, FechaFinal).ToString("yyyy-MM-dd")
+
+
+    'Dim ec As New ctiCalculo
+    'GVCalculoH.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-dd-MM"), Format(CDate(FechaFinal2), "yyyy-dd-MM"), 3)
+    '' GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd"), Format(CDate(FechaFinal), "yyyy-MM-dd"))
+
+    'ec = Nothing
+    'GVCalculoH.DataBind()
+
+
+
+    'GVCalculoH.EditIndex = e.NewEditIndex
+
+    ' End Sub
+    'Protected Sub GVCalculoH_RowDeleting(sender As Object, e As GridViewDeleteEventArgs) Handles GVCalculoH.RowDeleting
+    '    'Dim FechaFinal As Date
+    '    'Dim FechaFinal2 As Date
+    '    'FechaFinal = Convert.ToDateTime(TxFechaFin.Text)
+    '    'FechaFinal2 = DateAdd(DateInterval.Day, 1, FechaFinal).ToString("yyyy-MM-dd")
+
+
+    '    'Dim ec As New ctiCalculo
+    '    'GVCalculoH.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-dd-MM"), Format(CDate(FechaFinal2), "yyyy-dd-MM"), 3)
+    '    '' GridView1.DataSource = ec.gvChequeo(wucEmpleados2.idEmpleado, Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd"), Format(CDate(FechaFinal), "yyyy-MM-dd"))
+
+    '    'ec = Nothing
+    '    'GVCalculoH.DataBind()
+    '    'GVCalculoH.EditIndex = -1
+
+    'End Sub
+    Protected Sub btnReporte_Click(sender As Object, e As EventArgs) Handles btnReporte.Click
+
+
+        'Creación de un objeto CultureInfo con referencia cultural de México
+        Dim c As CultureInfo = New CultureInfo("es-MX")
+
+        reporte.ServerReport.Refresh()
+        If wucSucursales.idSucursal <> 0 Then
+            If TxFechaInicio.Text <> "" And TxFechaFin.Text <> "" Then
+                Dim dt1 As DateTime
+                dt1 = Format(CDate(TxFechaInicio.Text), "yyyy-MM-dd")
+                Dim dt2 As DateTime
+                dt2 = Format(CDate(TxFechaFin.Text), "yyyy-MM-dd")
+
+                Dim Htotales As Integer = 0
+                Dim HTrabajadas As Integer = 0
+
+                If TxHorasTrabajadas.Text <> "" Then
+                    HTrabajadas = TxHorasTrabajadas.Text
+                Else
+                    HTrabajadas = 0
+                End If
+
+                If TxHtotales.Text <> "" Then
+                    Htotales = TxHtotales.Text
+                Else
+                    Htotales = 0
+                End If
+
+                Dim p As New ReportParameter("Fech1", dt1)
+                reporte.LocalReport.SetParameters(p)
+
+                p = New ReportParameter("Fech2", dt2)
+                reporte.LocalReport.SetParameters(p)
+
+                p = New ReportParameter("idempleado", idEmpleadoTX.Text)
+                reporte.LocalReport.SetParameters(p)
+
+                'p = New ReportParameter("HTotales", Htotales)
+                'reporte.LocalReport.SetParameters(p)
+
+                'p = New ReportParameter("HTrabajadas", HTrabajadas)
+                'reporte.LocalReport.SetParameters(p)
+
+                reporte.ServerReport.Refresh()
+            Else
+                    Lmsg.Text = "Error: Debes capturar una fecha"
+            End If
+        Else
+            Lmsg.Text = "Error: Debes seleccionar una sucursal"
+        End If
+    End Sub
+
+
+    'Protected Sub GVCalculoH_RowUpdating(sender As Object, e As GridViewUpdateEventArgs) Handles GVCalculoH.RowUpdating
+
+    '    Dim cad1 As String = GVCalculoH.Rows(1).Cells(0).ToString()
+    '    Dim cad2 As String = GVCalculoH.Rows(1).Cells(3).ToString()
+
+
+    '    Dim ec As New ctiCalculo
+    '    GVCalculoH.DataSource = ec.gvAsigIncidencias(3,
+    '                                                  wucEmpleados2.idEmpleado, GVCalculoH.Rows(1).Cells(0).ToString,
+    '                                                 GVCalculoH.Rows(1).Cells(3).ToString)
+    '    'CInt(GridView1.Rows(Convert.ToInt32(e.CommandArgument)).Cells(0).Text))
+
+    '    ec = Nothing
+    '    GVCalculoH.DataBind()
+    'End Sub
+    'Protected Sub GVCalculoH_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles GVCalculoH.RowCommand
+    '    'If e.CommandName = "Update" Then
+    '    '    If IsNumeric(grdSR.Text) Then
+    '    '        GVCalculoH.Rows(Convert.ToInt32(grdSR.Text)).RowState = DataControlRowState.Normal
+    '    '        grdSR.Text = ""
+    '    '    End If
+
+
+    '    '    Dim sr As String
+
+    '    '    'sr = GVCalculoH.Rows(1).Cells(0).Text
+    '    '    'TxDDescasados.Text = sr
+
+    '    '    TxDDescasados.Text = GVCalculoH.SelectedRow.Cells(0).Text
+
+    '    'End If
+    'End Sub
+    Protected Sub GridView1_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles GridView1.RowCommand
+        If e.CommandName = "Editar" Then
+            If IsNumeric(grdSR.Text) Then
+                GridView1.Rows(Convert.ToInt32(grdSR.Text)).RowState = DataControlRowState.Normal
+                grdSR.Text = ""
+            End If
+
+
+
+            Dim dsP As New ctiCatalogos
+            Dim sr As String
+
+            sr = GridView1.Rows(Convert.ToString(e.CommandArgument)).Cells(3).Text
+
+            TxDDescasados.Text = sr
+        End If
+    End Sub
 End Class
